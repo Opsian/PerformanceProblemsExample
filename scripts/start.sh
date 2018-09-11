2d18e84b42cf81f5abe4548819fbb2fe9d829f49
Richard Warburton <richard.warburton@gmail.com>
2018-09-11 11:27:11 +0100

    fix startup script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-mvn package -DskipTests
+set -eu
 
 KEY=$1
 NAME=$2
 OPSIAN_PATH=$3
 
+cd .. && mvn package -DskipTests && cd scripts
+
 java \
 -agentpath:"$OPSIAN_PATH,apiKey=$KEY,debugLogPath=debug.log,agentId=$NAME" \
--jar ../target/dropwizard-example-1.4.0-SNAPSHOT.jar server ../example.yml
+-jar ../target/performance-problems-example-1.3.5.jar server ../example.yml
 
