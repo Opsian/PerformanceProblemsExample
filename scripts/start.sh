4acf29c90ea50f09ea4faf445a8067e18e431577
Richard Warburton <richard.warburton@gmail.com>
2019-05-02 10:39:56 +0100

    old changes

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,8 @@ OPSIAN_AGENT=$3
 
 # mvn package -DskipTests
 
+# TODO: add application version to make it easier to demo / diff
+
 java \
 -agentpath:"${OPSIAN_AGENT}apiKey=$KEY,debugLogPath=debug.log,agentId=$NAME" \
 -jar target/performance-problems-example-1.3.5.jar server example.yml
