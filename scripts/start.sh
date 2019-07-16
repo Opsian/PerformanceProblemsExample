e850aa7e6da7c490712dcbdaf3a137736aaa92cd
Sadiq <sadiq@toao.com>
2019-07-16 17:36:59 +0100

    add application version to the startup parameters

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,10 +8,8 @@ OPSIAN_AGENT=$3
 
 mvn package -DskipTests
 
-# TODO: add application version to make it easier to demo / diff
-
 java \
 -Xmx1024M \
--agentpath:"${OPSIAN_AGENT}apiKey=$KEY,debugLogPath=opsian-debug.log,agentId=$NAME" \
+-agentpath:"${OPSIAN_AGENT}apiKey=$KEY,debugLogPath=opsian-debug.log,agentId=$NAME,applicationVersion=$NAME" \
 -jar target/performance-problems-example-1.3.5.jar server example.yml
 
