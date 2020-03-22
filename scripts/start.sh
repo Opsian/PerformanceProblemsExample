9b569dd18a64c2fdacb6382cc72bc6ccbfaf37e5
Richard Warburton <richard.warburton@gmail.com>
2020-03-22 16:13:31 +0000

    reduce logging when not runnign as a demo

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,5 +11,5 @@ mvn package -DskipTests
 java \
 -Xmx1024M \
 -agentpath:"${OPSIAN_AGENT}apiKey=$KEY,debugLogPath=opsian-debug.log,agentId=$NAME,applicationVersion=$NAME" \
--jar target/performance-problems-example-1.3.5.jar server example.yml
+-jar target/performance-problems-example-1.3.5.jar server demo.yml
 
